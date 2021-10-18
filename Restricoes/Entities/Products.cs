ab1f57d984012c3ea5e354cd4cd0ff47b0622e30
Pedro Lucas <pedro.lima3@estudante.ifms.edu.br>
2021-10-18 00:04:58 -0300

    restricoes

---
+++ Restricoes/Entities/Products.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Restricoes.Entities
+{
+    class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Product))
+            {
+                throw new ArgumentException("Comparring error: argument is not an Product");
+            }
+
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Restricoes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Restricoes.Entities;
+using Restricoes.Services;
+
+namespace Restricoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vector = Console.ReadLine().Split(',');
+                double price = double.Parse(vector[1], CultureInfo.InvariantCulture);
+
+                list.Add(new Product(vector[0], price));
+            }
+
+            CalculationService calculations = new CalculationService();
+
+            Product p = calculations.Max(list);
+
+            Console.Write("Most Expensive: ");
+            Console.Write(p);
+        }
+    }
+}
